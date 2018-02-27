0b69865bd41a9ebc51b7306facbc1d344121738c
Francisco Ari Josino <arijr@outlook.com>
2018-02-27 01:35:23 -0500

    reorganized dependencies and updated webpack to version 4

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,19 @@
 module.exports = {
   entry: './frontend/src/app.js',
   output: {
+    path:__dirname+'/',
     filename: './frontend/bundle.js'
   },
+  devtool:'source-map',
   module:{
-    loaders: [
+    rules: [
       {
         test: /\.vue$/,
         loader: 'vue-loader',
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
       }
     ]
   }
