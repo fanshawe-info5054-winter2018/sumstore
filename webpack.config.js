a8da77793483efee668c0ee62fb9cbacbbff0f84
Francisco Ari Josino Junior <arijr@outlook.com>
2018-03-02 00:39:46 -0500

    added support for IE and older 'browsers'

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  entry: './frontend/src/app.js',
+  entry: ['babel-polyfill','./frontend/src/app.js'],
   output: {
     path:__dirname+'/',
     filename: './frontend/bundle.js'
@@ -14,7 +14,14 @@ module.exports = {
       {
         test: /\.css$/,
         use: ['style-loader', 'css-loader']
+      },
+      {
+      test: /\.js$/,
+      exclude: /(node_modules)/,
+      use: {
+        loader: 'babel-loader',
       }
+    }
     ]
   }
 };
