90b38ebb7b757bef296c808bd5ffd0c8b1a08df0
Isabela Bastos <mb.isabela@gmail.com>
2018-03-17 14:07:28 -0400

    TA-10 - Added filtering options for user gamelist

--- frontend/src/view/mainpage/home.vue
+++ frontend/src/view/mainpage/home.vue
@@ -1,49 +1,75 @@
 <template>
-<div>
+  <div>
 
-  <div id="carouselExampleIndicators" class="carousel slide my-4 col-lg-9" data-ride="carousel">
-    <ol class="carousel-indicators">
-      <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-      <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-      <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-    </ol>
-    <div class="carousel-inner" role="listbox">
-      <div class="carousel-item active">
-        <img class="d-block img-fluid" src="http://placehold.it/900x350" alt="First slide">
+    <div id="carouselExampleIndicators" class="carousel slide my-4 col-lg-9" data-ride="carousel">
+      <ol class="carousel-indicators">
+        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+      </ol>
+      <div class="carousel-inner" role="listbox">
+        <div class="carousel-item active">
+          <img class="d-block img-fluid" src="http://placehold.it/900x350" alt="First slide">
+        </div>
+        <div class="carousel-item">
+          <img class="d-block img-fluid" src="http://placehold.it/900x350" alt="Second slide">
+        </div>
+        <div class="carousel-item">
+          <img class="d-block img-fluid" src="http://placehold.it/900x350" alt="Third slide">
+        </div>
       </div>
-      <div class="carousel-item">
-        <img class="d-block img-fluid" src="http://placehold.it/900x350" alt="Second slide">
-      </div>
-      <div class="carousel-item">
-        <img class="d-block img-fluid" src="http://placehold.it/900x350" alt="Third slide">
-      </div>
+      <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+      </a>
     </div>
-    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-      <span class="sr-only">Previous</span>
-    </a>
-    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-      <span class="carousel-control-next-icon" aria-hidden="true"></span>
-      <span class="sr-only">Next</span>
-    </a>
+    <div>
+      <input type="text" v-model="search" placeholder="Search for a game title"/>
+      <select v-model="splatform">
+        <option value="">Select a Platform</option>
+        <option v-for="platform in platformlist" :key="platform.name">{{platform.name}}</option>
+      </select>
+    </div>
+    <ul class="list-group">
+      <li class="list-group-item" v-for="game in gamelist">
+        <h3>
+          {{game.name}}
+        </h3>
+        platform: {{game.platform.name}}
+      </li>
+    </ul>
   </div>
-
-  <ul class="list-group">
-    <li class="list-group-item" v-for="game in gamelist">
-      <h3>
-        {{game.name}}
-      </h3>
-      platform: {{game.platform.name}}
-    </li>
-  </ul>
-</div>
 </template>
 <script>
-export default {
-  computed:{
-    gamelist(){
-      return this.$store.getters["game/games"];
+    export default {
+
+        // Initialize filter form elements.
+        data() {
+            return {
+                search: '',
+                splatform: '',
+            }
+        },
+        computed: {
+            gamelist(){
+                // Store this for use in filter functions.
+                var self = this;
+                return this.$store.getters["game/games"].filter(function (item) {
+                    // Filters games by Title.
+                    return item.name.toLowerCase().indexOf(self.search.toLowerCase()) >= 0;
+                }).filter(function (item) {
+                    // Filters games by platform.
+                    return self.splatform == '' || item.platform.name == self.splatform;
+                });
+            },
+            platformlist(){
+                return this.$store.getters["platform/platforms"];
+            }
+
+        }
     }
-  }
-}
 </script>
