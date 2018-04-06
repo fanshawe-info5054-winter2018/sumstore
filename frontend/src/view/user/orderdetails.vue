c6949cf50b0396ce379b3edd0fd8331abf9d91ed
Francisco Ari Josino <arijr@outlook.com>
2018-04-06 17:58:40 -0400

    added some style to the orderetails page

--- frontend/src/view/user/orderdetails.vue
+++ frontend/src/view/user/orderdetails.vue
@@ -3,15 +3,17 @@
   <br>
   <h2>Order Details</h2>
   <br>
-  <h3>Order status: {{order.status}}</h3>
-  <ul>
-    <li v-for="product in order.products">
-      <span>Order Number: {{order.number}}</span></br>
+  <h3>Order Number: {{order.number}}</h3>
+  <h4>Status: <span class="badge badge-primary">{{order.status}}</span></h4>
+  <div class="clearfix">
+    <h4 class="float-left">Tracking #: {{order.tracking}}</h4>
+    <h4 class= "float-right">Date Purchased: {{order.date}}</h4>
+  </div>
+  <ul class="list-group">
+    <li class="list-group-item" v-for="product in order.products">
       <span>Game Title: {{product.game.name}}</span></br>
       <span>Subtotal: ${{product.game.price}}</span></br>
       <span>Total: ${{order.price}}</span></br>
-      <span>Date Purchased: {{order.date}}</span></br>
-      <span>Tracking #: {{order.tracking}}</span>
     </li>
   </ul>
 </div>
